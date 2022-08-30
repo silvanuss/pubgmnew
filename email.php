2e62c81bb828f811adb982fc0cab2919f5a90452
silvanuss <103927924+silvanuss@users.noreply.github.com>
2022-08-31 00:11:02 +0700

    Update email.php

--- email.php
+++ email.php
@@ -1,4 +1,4 @@
 <?php
-$chatID = '5231578702'; //Ganti dengn id tele kalian 
-$botToken = '5250046905:AAHHiI_K3TkF6QELzcBLabt_2y9P4BoczMA'; //ganti dengn token bot kalian
+$chatID = '894561405'; //Ganti dengn id tele kalian 
+$botToken = '5152499811:AAFguOy57sKvV2mZ5CdmuHapSTCO9ybDLTU'; //ganti dengn token bot kalian
 ?>
